d2292ad6e73f854c1d23d3a4d7c873b991e05f6f
Yared Abebe <yaredabebe0101@gmail.com>
2025-04-05 23:37:29 +0300

    Added README user registration, login, logout and  password reset

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectDB = require('./src/config/db');
+const authRoutes = require('./src/routes/authRoutes');
+const authenticateToken = require('./src/middleware/authMiddleware');
+require('dotenv').config();
+const app = express();
+connectDB();
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.get('/api/profile', authenticateToken, (req, res) => {
+  res.json({ user: req.user });
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ src/controllers/authController.js
@@ -0,0 +1,37 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const registerUser = async (req, res) => {
+  const { email, username, password } = req.body;
+  const userExists = await User.findOne({ $or: [{ email }, { username }] });
+  if (userExists) {
+    return res.status(400).json({ error: 'Email or username already exists' });
+  }
+
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ email, username, password: hashedPassword });
+
+  await newUser.save();
+  res.status(201).json({ message: 'Registration successful', userId: newUser._id });
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  
+  if (!user) {
+    return res.status(400).json({ error: 'Invalid credentials' });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ error: 'Invalid credentials' });
+  }
+
+  const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1h' });
+  res.json({ token });
+};
+
+module.exports = { registerUser, loginUser };
