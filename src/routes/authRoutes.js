ea3c335da257ed0f24500e6d7b909f44e713200d
YaredAbebe <149156568+YaredAbebe01@users.noreply.github.com>
2025-04-06 12:01:21 +0300

    Update authRoutes.js
    Logout route added

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/authController');
+const authenticateToken = require('../middleware/authMiddleware');
 const router = express.Router();
-
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/logout', authenticateToken, (req, res) => {
+  res.json({ message: 'Successfully logged out' });
+});
 
 module.exports = router;
